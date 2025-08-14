90bd7348bcf1e932d0b603d26473e4473d297f6b
Nathan <nathan.singh@inreachsw.com>
2025-08-14 10:13:05 +1000

    Continue refactor, require user to sign in for spotify

--- BreadCharts.Web/Data/ApplicationUser.cs
+++ BreadCharts.Web/Data/ApplicationUser.cs
@@ -8,6 +8,6 @@ public class ApplicationUser : IdentityUser
     public string RefreshToken { get; set; } = "";
     
     public string AccessToken { get; set; } = "";
-    
-    public string ThirdPartyId { get; set; }
+
+    public string ThirdPartyId { get; set; } = "";
 }

c6425f6354e1f4139cfe7fabe8f0ca96e4cd92dd
Nathan <nathan.singh@inreachsw.com>
2025-08-14 13:24:31 +1000

    Styling + search sort

--- BreadCharts.Core/Models/ChartOption.cs
+++ BreadCharts.Core/Models/ChartOption.cs
@@ -7,6 +7,11 @@ public class ChartOption
     public string Name { get; set; }
     
     public ChartOptionType Type { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 }
 
 public enum ChartOptionType

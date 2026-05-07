0af5f6e9801d8054a7c146e16889fbd54e0733af
Nathan <nathan.singh@inreachsw.com>
2026-05-08 09:00:22 +1000

    mid  update

--- BreadCharts.Web/Data/ApplicationUser.cs
+++ BreadCharts.Web/Data/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace BreadCharts.Web.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string DisplayName { get; set; } = "";
+    
     public string ThirdPartyId { get; set; } = "";
 }

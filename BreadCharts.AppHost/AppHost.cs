4f45dc780ec607805a7bea19322aed9bcb90bf89
NahFarn <nathan.singh@inreachsw.com>
2026-05-24 10:38:33 +1000

    avalonia and web api orchestrated in AppHost

--- BreadCharts.AppHost/AppHost.cs
+++ BreadCharts.AppHost/AppHost.cs
@@ -1,10 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var web = builder.AddProject<Projects.BreadCharts_Web>("breadcharts-web");
-web
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithEnvironment("DOTNET_ENVIRONMENT", "Development")
-    .WithEnvironment("Spotify__ClientId", "7412007f28e045bf90c021c079e92655")
-    .WithEnvironment("Spotify__ClientSecret", "29b610aae47944668afc79084a856f0b");
+// var web = builder.AddProject<Projects.BreadCharts_Web>("breadcharts-web");
+// web
+//     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+//     .WithEnvironment("DOTNET_ENVIRONMENT", "Development")
+//     .WithEnvironment("Spotify__ClientId", "7412007f28e045bf90c021c079e92655")
+//     .WithEnvironment("Spotify__ClientSecret", "29b610aae47944668afc79084a856f0b");
+
+var api = builder.AddProject<Projects.BreadCharts_WebApi>("breadcharts-api");
+var desktop = builder.AddProject<Projects.BreadCharts_Avalonia_Desktop>("breadcharts-avalonia");
 
 builder.Build().Run();

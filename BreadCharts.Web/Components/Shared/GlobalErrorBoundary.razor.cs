7a5b8a11389edb99acc3fdf40be0f868ccfdc5d4
NahFarn <nathan.singh@inreachsw.com>
2026-05-25 09:38:24 +1000

    remove old blazor app

--- BreadCharts.Web/Components/Shared/GlobalErrorBoundary.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace BreadCharts.Web.Components.Shared
-{
-    // Code-behind intentionally minimal; logic is implemented in the .razor file.
-    public partial class GlobalErrorBoundary : ComponentBase
-    {
-    }
-}
